6d2c38ac975ccada6c757882ff418d84e7f0fc3b
bvottri <bruna.votre@gmail.com>
2021-05-19 16:06:18 -0300

    Exercícios Parte III

---
+++ fundamentos/bloco_4/aula_1/parteIII.js
@@ -0,0 +1,13 @@
+let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+let base = 5;
+let altura = 8;
+let area = base * altura;
+let perimetro = (2 * base) + (2 * altura)
+console.log (area, perimetro)
