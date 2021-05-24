1e66a5492b61a2ee4228a8310f15055a73344214
bvottri <bruna.votre@gmail.com>
2021-05-24 13:23:14 -0300

    Exercícios 7,8,9

--- fundamentos/bloco_4/aula_1/Exercícios.js
+++ fundamentos/bloco_4/aula_1/Exercícios.js
@@ -21,9 +21,9 @@ let a = 1;
 let b = 2;
 
 if (a>b) {
-    console.log(a);
+  console.log(a);
 } else {
-    console.log(b);
+  console.log(b);
 }
 
 //3-Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
@@ -33,12 +33,12 @@ let b = 2;
 let c = 3;
 
 if (a>b && a>c) {
-    console.log(a);
+  console.log(a);
 } 
 else if (b>a && b>c) {
-    console.log(b);
+  console.log(b);
 } else {
-    console.log (c);
+  console.log (c);
 }
 
 //4-Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
@@ -46,11 +46,11 @@ else if (b>a && b>c) {
 let valor = 1;
 
 if (valor > 0) {
-    console.log('positive');
+  console.log('positive');
 } else if (valor < 0) {
-    console.log('negative');
+  console.log('negative');
 } else {
-    console.log('zero');
+  console.log('zero');
 }
 
 //5-Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem 
@@ -62,17 +62,62 @@ let c = -4;
 let sum = a+b+c
 
 if (sum==180 && a>0 && b>0 && c>0) {
-    console.log(true)
+  console.log(true)
 } else {
-    console.log(false)
+  console.log(false)
 }
 
 //6-Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
 
-let piece = 'King';
+let piece = 'PAWN';
 
-if (piece == 'King') {
-    console.log('Qualquer direção')
-}else {
-    console.log('Erro')
+switch (piece.toLowerCase()) {
+  case 'king':
+    console.log('Qualquer direção');
+    break;
+  case 'queen':
+    console.log('Qualquer direção');
+    break;
+  case 'bishop':
+    console.log('Diagonais');
+    break;
+  case 'knight':
+    console.log('L');
+    break;
+  case 'rook':
+    console.log('Verticais e Horizontais');
+    break;
+  case 'pawn':
+    console.log('Verticais e Diagonais');
+    break;
+  default:
+    console.log("Erro")
 }
+
+//7-Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+
+let nota = 49;
+let conceito = 0;
+
+if (nota >= 90) {
+  console.log(conceito = 'A');
+} else if (nota >= 80) {
+  console.log(conceito = 'B');
+} else if (nota >= 70) {
+  console.log(conceito = 'C');
+} else if (nota >= 60) {
+  console.log(conceito = 'D');
+} else if (nota >= 50) {
+  console.log(conceito = 'E');
+} else if (nota < 50) {
+  console.log(conceito = 'F');
+} else {
+  console.log('Erro');
+}
+
+//8-Escreva um programa que defina três números em variáveis e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false.
+
+let number1 = 0;
+let number2 = 0;
+let number2 = 0;
+
