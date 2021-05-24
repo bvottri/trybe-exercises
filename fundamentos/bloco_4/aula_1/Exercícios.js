8c7f8ff7a3d4ed559b5acc1ca5059933e1306fb3
bvottri <bruna.votre@gmail.com>
2021-05-24 18:49:01 -0300

    Exercícios 10 e 11

--- fundamentos/bloco_4/aula_1/Exercícios.js
+++ fundamentos/bloco_4/aula_1/Exercícios.js
@@ -117,7 +117,80 @@ if (nota >= 90) {
 
 //8-Escreva um programa que defina três números em variáveis e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false.
 
-let number1 = 0;
-let number2 = 0;
-let number2 = 0;
+let number1 = 1;
+let number2 = 3;
+let number3 = 3;
+let resultado = 0;
 
+if (number1 % 2 === 0 || number2 % 2 === 0 || number3 % 2 === 0) {
+  console.log(resultado = true)
+} else {
+  console.log(resultado = false)
+}
+
+//9-Escreva um programa que defina três números em variáveis e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false.
+
+let number1 = 2;
+let number2 = 4;
+let number3 = 6;
+let resultado = 0;
+
+if (number1 % 2 !== 0 || number2 % 2 !== 0 || number3 % 2 !== 0) {
+  console.log(resultado = true)
+} else {
+  console.log(resultado = false)
+}
+
+//10-Escreva um programa que se inicie com dois valores em duas variáveis diferentes: o custo de um produto e seu valor de venda. 
+//A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+
+let custoProduto = 0;
+let vendaProduto = 200;
+let imposto = custoProduto * 0.2;
+let custoTotal = custoProduto + imposto;
+let lucro = vendaProduto - custoTotal;
+
+let numeroDeProdutosVendidos = 1000;
+
+let lucroProdutos = numeroDeProdutosVendidos * lucro;
+
+if (custoProduto & vendaProduto >= 0) {
+  console.log(lucroProdutos);
+} else {
+  console.log('Erro');
+}
+
+//11-Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. 
+//Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
+
+salarioBruto = 3000;
+inss = 0;
+ir = 0;
+
+if (salarioBruto <= 1566.94) {
+  inss = 0.08 * salarioBruto
+} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+  inss = 0.09 * salarioBruto
+} else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+  inss = 0.11 * salarioBruto 
+} else {
+  inss = 570.88
+}
+
+salarioBase = salarioBruto - inss
+
+if (salarioBase <= 1903.98) {
+  inss = 0
+} else if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
+  ir = (salarioBase * 0.075) - 142.8
+} else if (salarioBase >= 2826.66 && salarioBase <= 3751.05) {
+  ir = (salarioBase * 0.115) - 354.8
+} else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+  ir = (salarioBase * 0.225) - 636.13
+} else {
+  ir = (salarioBase * 0.275) - 839.36
+}
+
+salarioLiquido = salarioBase - ir
+
+console.log(salarioLiquido)
